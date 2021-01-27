19c139e29b63109049b9d995433800fa1ef87443
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-27 14:17:47 -0500

    Creating the SignUp method in controller to register new users

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,10 +1,24 @@
-import { Request,Response } from 'express'
-import User,{IUser} from '../../models/User'
+import { Request, Response } from "express";
+import User, { IUser } from "../../models/User";
 
-export const signUp = (req: Request,res: Response)=>{
+export const signUp = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .json({ msg: "Please, send your email and password." });
+  }
 
-}
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({ msg: "User already exixts!" });
+  }
 
-export const signIn = (req: Request,res: Response)=>{
+  const newUser = new User(req.body);
+  await newUser.save();
+  return res.status(201).json(newUser);
+};
 
-}
+export const signIn = (req: Request, res: Response) => {};
