2f9059e751d5494c706fc87f83a3bfb1fd5998d9
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-27 14:56:46 -0500

    Creating the SignIn method in controller to validate users

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,5 +1,22 @@
+import config from "../../config/config";
+
 import { Request, Response } from "express";
 import User, { IUser } from "../../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+function createToken(user: IUser) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    config.secretKey,
+    {
+      expiresIn: 60 * 60 * 24,
+    }
+  );
+}
 
 export const signUp = async (
   req: Request,
@@ -21,4 +38,25 @@ export const signUp = async (
   return res.status(201).json(newUser);
 };
 
-export const signIn = (req: Request, res: Response) => {};
+export const signIn = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .json({ msg: "Please, send your email and password." });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({ msg: "User does not exixts!" });
+  }
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+  if (isMatch) {
+    return res.status(201).json({ token: createToken(user) });
+  }
+
+  return res.status(400).json({ msg: "The email or password are incorrect!" });
+};
