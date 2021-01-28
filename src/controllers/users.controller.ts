da990818215856da20660cfe9f1a997a23ccfb6b
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-27 20:11:16 -0500

    Apply and configure methods user controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,10 +2,13 @@ import { Request, Response } from 'express'
 
 import User from '../models/User'
 
-export const getUsers = (req: Request,res: Response)=>{
-
+export const getUsers = async (req: Request,res: Response)=>{
+  const users = await User.find({})
+  res.status(200).json(users)
 }
 
-export const getUser = (req: Request,res: Response)=>{
-
+export const getUser = async (req: Request,res: Response)=>{
+  const {id} = req.params
+  const user = await User.findById(id)
+  res.status(200).json(user)
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getUsers, getUser } from "../controllers/users.controller";
+import passport from "passport";
 
 const router = Router();
 
-router.get("/", getUsers);
-router.get("/:id", getUser);
+router.get("/", passport.authenticate("jwt", { session: false }), getUsers);
+router.get("/:id", passport.authenticate("jwt", { session: false }), getUser);
 
 export default router;
